59c70e1099e36606e7a1e661598563ba2214e5ff
ares@byteit-online.com <ares@byteit-online.com@41142a6e-483b-0410-9a39-79d5471c0866>
2007-10-07 19:56:41 +0000

    Set units defined

--- starv/gui/workingVersion/src/OptionsMenu.java
+++ starv/gui/workingVersion/src/OptionsMenu.java
@@ -1,5 +1,8 @@
 import java.awt.MenuItem;
 import java.awt.Menu;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.MenuItem.*;
 
 public class OptionsMenu extends MenuHandler {
 
@@ -13,9 +16,40 @@ public class OptionsMenu extends MenuHandler {
 		subMenu.add(menu = new MenuItem("Set Size"));
 		menu.addActionListener(this);
 		
+		Menu submenu2 = new Menu("Set Units");
 		
-		subMenu.add(menu = new MenuItem("Set Units"));
-		menu.addActionListener(this);
+		ButtonGroup group = new ButtonGroup();
+		CheckboxMenuItem radioMenu = new CheckboxMenuItem("Meters");
+		radioMenu.addActionListener(this);
+		radioMenu.setState(true);
+		
+		submenu2.add(radioMenu);
+		
+		radioMenu = new CheckboxMenuItem("Kilometers");
+		radioMenu.addActionListener(this);
+		
+		submenu2.add(radioMenu);
+		
+		submenu2.addSeparator();
+		
+
+		radioMenu = new CheckboxMenuItem("Feet");
+		radioMenu.addActionListener(this);
+
+		
+		submenu2.add(radioMenu);
+		
+		radioMenu = new CheckboxMenuItem("Miles");
+		radioMenu.addActionListener(this);
+
+		
+		
+		
+		radioMenu = new CheckboxMenuItem("Imperial");
+		radioMenu.addActionListener(this);
+		submenu2.add(radioMenu);
+		
+		subMenu.add(submenu2);
 		
 		add(subMenu);
 		
