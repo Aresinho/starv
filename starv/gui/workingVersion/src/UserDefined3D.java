872f57c455916768e2f484db014c4b6c5126355f
nintendo.hack <nintendo.hack@41142a6e-483b-0410-9a39-79d5471c0866>
2007-10-07 17:38:48 +0000

    Updateing

---
+++ starv/gui/workingVersion/src/UserDefined3D.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Vector3d;
+
+
+public class UserDefined3D extends Object3D{
+	public LinkedList objects=null;
+	
+	public UserDefined3D()
+	{
+		super();
+	}
+	public TransformGroup drawMe(Vector3d newVector)
+	{
+		TransformGroup holder = new TransformGroup();
+		for(int k = 0;k< objects.size();k++)
+		{
+			Object3D item = (Object3D)objects.get(k);
+			holder.addChild(item.drawMe(newVector));
+		}
+		return holder;
+	}
+
+}
