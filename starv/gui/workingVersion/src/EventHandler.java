5f905fcb3ad28e3a1f32e667dafeaa28ef850168
ares@byteit-online.com <ares@byteit-online.com@41142a6e-483b-0410-9a39-79d5471c0866>
2007-10-07 19:49:51 +0000

    Popup for set Size

--- starv/gui/workingVersion/src/EventHandler.java
+++ starv/gui/workingVersion/src/EventHandler.java
@@ -1,15 +1,25 @@
 import java.awt.*;
 import java.util.LinkedList;
+import java.awt.event.*;
+import java.awt.Toolkit.*;
 
 import javax.swing.*;
 
 import javax.swing.JComponent;
 
-public class EventHandler {
+public class EventHandler implements ActionListener {
 	
-	JComponent newContentPane;
+	
+	
+	/**
+	 * Internal Frame for pop up and get feedback/input from user
+	 * @author Ares
+	 */
 	JFrame popUp = new JFrame("Set Size");
-
+	JComponent newContentPane;
+	JTextField reader = new JTextField(30);
+	JTextField reader2 = new JTextField(30);
+	
 	public void findAndExecute(String event)
 	{
 		if(event.equalsIgnoreCase("exit"))
@@ -21,21 +31,67 @@ public class EventHandler {
 			
 		}
 		
-		if(event.equals("Map..."))
+		if("Map...".equals(event))
 		{
 			redraw(Context.EDITOR);
-			System.out.println( " woop ?");
 		}
-		if(event.equalsIgnoreCase("trace"))
+		if("trace".equalsIgnoreCase(event))
 		{
 			redraw(Context.TRACE);
 		}
-		if(event.equalsIgnoreCase("start trace"))
+		if("start trace".equalsIgnoreCase(event))
 		{
 			TraceWindow.t.start();
 		}
 		if(event.equalsIgnoreCase("set size"))
 		{
+			JPanel panel = new JPanel();
+			panel.setLayout(new BoxLayout(panel,BoxLayout.PAGE_AXIS));
+			
+			JPanel messagex = new JPanel();
+			messagex.setLayout(new BoxLayout(messagex,BoxLayout.X_AXIS));
+			panel.setBorder(BorderFactory.createEmptyBorder(30,15,30,15));
+		
+			messagex.add(new JLabel("Enter desired X-Size: "));
+			reader.setPreferredSize(new Dimension(50,25));
+			messagex.add(reader);
+			
+			
+			
+			JPanel messageY = new JPanel();
+			messageY.setLayout(new BoxLayout(messageY,BoxLayout.X_AXIS));
+			messageY.add(new JLabel("Enter desired Y-Size: "));
+			
+			
+			
+			messageY.add(reader2);
+			
+			panel.setPreferredSize(new Dimension(260, 120));
+			
+			JButton ok = new JButton("Ok");
+			ok.setActionCommand("setSize");
+			JButton cancel = new JButton("Cancel");
+			
+			ok.addActionListener(this);
+			cancel.setActionCommand("cancelSize");
+			cancel.addActionListener(this);
+			
+			
+			JPanel decision = new JPanel();
+			decision.setLayout(new BoxLayout(decision, BoxLayout.X_AXIS));
+			decision.add(cancel);
+			decision.add(ok);
+			
+			panel.add(messagex);
+			panel.add(messageY);
+			panel.add(decision);
+			
+			Toolkit toolkit = java.awt.Toolkit.getDefaultToolkit ();
+			Dimension screensize = toolkit.getScreenSize ();
+			
+			popUp = new JFrame("Set Size");
+			popUp.setContentPane(panel);
+			popUp.setLocation(screensize.width / 2 - 130, screensize.height / 2 - 60);
 			popUp.pack();
 			popUp.setVisible(true);
 		}
@@ -135,6 +191,27 @@ public class EventHandler {
 		MainSystem.frame.pack();
 	}
 	
+	
+	public void actionPerformed(ActionEvent item) 
+	{ 
+	    if("ok".equalsIgnoreCase(item.getActionCommand()))
+	    {
+	    	//call method to change size
+	    }
+	    if("cancelSize".equalsIgnoreCase(item.getActionCommand()))
+	    {
+	    	popUp.dispose();
+	    }
+	}
+	
+	
+	/**
+	 * Enumerable class to keep track of the different context of the program such as
+	 * MAIN, EDITO, TRACE - if more contexts are added to application then they should be 
+	 * added in here. 
+	 * @author Ares
+	 * 
+	 */
 	public enum Context {
 	    MAIN, EDITOR, TRACE 
 	}
