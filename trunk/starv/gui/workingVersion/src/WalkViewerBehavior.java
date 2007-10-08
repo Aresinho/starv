dee101b47d3d181964c038e8bdfb6552bc2d36af
ares@byteit-online.com <ares@byteit-online.com@41142a6e-483b-0410-9a39-79d5471c0866>
2007-10-08 16:44:34 +0000

    Cleanup and Comments

--- trunk/starv/gui/workingVersion/src/WalkViewerBehavior.java
+++ trunk/starv/gui/workingVersion/src/WalkViewerBehavior.java
@@ -1,49 +1,22 @@
-import java.applet.Applet;
-import java.awt.AWTEvent;
-import java.awt.BorderLayout;
-import java.awt.CheckboxMenuItem;
+
 import java.awt.Component;
 import java.awt.Cursor;
-import java.awt.Frame;
-import java.awt.Menu;
-import java.awt.MenuBar;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.io.File;
-import java.util.Enumeration;
-import java.util.EventListener;
 
-import javax.media.j3d.Behavior;
-import javax.media.j3d.BoundingSphere;
-import javax.media.j3d.BranchGroup;
-import javax.media.j3d.Canvas3D;
-import javax.media.j3d.DirectionalLight;
-import javax.media.j3d.Group;
-import javax.media.j3d.Light;
-import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.media.j3d.WakeupCriterion;
 import javax.media.j3d.WakeupOnAWTEvent;
 import javax.media.j3d.WakeupOnElapsedFrames;
 import javax.media.j3d.WakeupOr;
-import javax.vecmath.Color3f;
-import javax.vecmath.Matrix4d;
-import javax.vecmath.Point3d;
-import javax.vecmath.Point3f;
-import javax.vecmath.Vector3d;
-import javax.vecmath.Vector3f;
 
-import com.sun.j3d.utils.universe.PlatformGeometry;
-import com.sun.j3d.utils.universe.SimpleUniverse;
-import com.sun.j3d.utils.universe.Viewer;
-import com.sun.j3d.utils.universe.ViewingPlatform;
-
+/**
+ * Encapsulates all three behavioirs into one in order to enforce a specific Walk 
+ * Behavior. Supports set/get of the rotation and translation factors that control
+ * the speed of movement. Supports the Control modifier as an alternative to the
+ * Meta modifier.
+ * @author Ares
+ *
+ */
 class WalkViewerBehavior extends ViewerBehavior {
   // This class is inspired by the MouseBehavior, MouseRotate,
   // MouseTranslate, and MouseZoom utility behaviors provided with
@@ -455,6 +428,7 @@ class WalkViewerBehavior extends ViewerBehavior {
    *            the MouseEvent to respond to
    */
   public void onButton3(MouseEvent mev) {
+   System.out.print(mev.getButton());
    System.out.println( " mouse three!");
 	  if (subjectTransformGroup == null)
       return;
@@ -524,20 +498,7 @@ class WalkViewerBehavior extends ViewerBehavior {
 
     // Update the transform group
     subjectTransformGroup.setTransform(currentTransform);
-    System.out.println(currentTransform.toString());
     previousX = x;
     previousY = y;
   }
 }
-
-//
-//CLASS
-//CheckboxMenu - build a menu of grouped checkboxes
-//
-//DESCRIPTION
-//The class creates a menu with one or more CheckboxMenuItem's
-//and monitors that menu. When a menu checkbox is picked, the
-//previous one is turned off (in radio-button style). Then,
-//a given listener's checkboxChanged method is called, passing it
-//the menu and the item checked.
-//
