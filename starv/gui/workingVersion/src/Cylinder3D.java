959a8a10aa2190e76c64fd62af4d23d83dc59e56
chamuco23@gmail.com <chamuco23@gmail.com@41142a6e-483b-0410-9a39-79d5471c0866>
2007-10-09 02:47:23 +0000

    add javadoc documentation

--- starv/gui/workingVersion/src/Cylinder3D.java
+++ starv/gui/workingVersion/src/Cylinder3D.java
@@ -4,15 +4,29 @@ import javax.vecmath.Vector3d;
 import com.sun.corba.se.impl.orbutil.graph.Node;
 import com.sun.j3d.utils.geometry.Cylinder;
 
-
+/**
+ * 
+ * @author IPAPI
+ *
+ */
 public class Cylinder3D extends Object3D {
 	float radius =1;
 	float length=1;
 	public static Vector3d translation;
+	
+	
+	/**
+	 * Constructor using the Object3D contructor
+	 */
 	public Cylinder3D()
 	{
 		super();
 	}
+	
+	/**
+	 *  draws a cylinder
+	 *  @return group   a TransformGroup object
+	 */
 	public TransformGroup drawMe()
 	{
 		transform3d.setTranslation(translation);
@@ -20,25 +34,47 @@ public class Cylinder3D extends Object3D {
 		group.addChild(new Cylinder(radius,length));
 		return group;
 	}
+	
+	/**
+	 *  sets the radius of the cylinder.
+	 * @param radius
+	 */
 	public void setRadius(float radius)
 	{
 		this.radius = radius;
 	}
+	
+	/**
+	 * sets the length of the cylinder
+	 * @param length
+	 */
 	public void setLength(float length)
 	{
 		this.length = length;
 	}
+	
+	/**
+	 *  returns the radius of this cylinder
+	 * @return this.radius 
+	 */
 	public float getRadius()
 	{
 		return this.radius;
 	}
-	//public float
+	
+	/**
+	 *  sets the vector to possition the cylinder.
+	 * @param vec
+	 */
 	public void setVector(Vector3d vec)
-	{/*
-		Probally pass the postion of the mouse, then figure out some algortim to translate that into valeus of vector3d
-	*/translation = vec;
-		
+	{
+		translation = vec;
 	}
+	
+	/**
+	 * returns the vector of the cylinder
+	 * @return translation  a Vector3D object
+	 */
 	public Vector3d getVector()
 	{
 		return translation;
